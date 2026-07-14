fcae91f50d6b3620ee1a87da9ae26d7a89188ff9
Rithesh <ritheshputtur15@gmail.com>
2026-07-14 21:15:30 +0530

    Time: 6 ms (70.21%), Space: 46.8 MB (14.06%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -15,7 +15,24 @@ class Solution {
             max=Math.max(max,r-l+1);
         }
         return max;
-        
-        
+
+
+        // int max=0;
+        // int left=0;
+        // int right=0;
+        // while (right<s.length()){
+        //     char ch=s.charAt(right);
+        //     if(!hs.contains(ch)){
+        //         hs.add(ch);
+        //         max=Math.max(max,right-left+1);
+        //         right++;
+        //     }
+        //     else {
+        //         hs.remove(s.charAt(left));
+        //         left++;
+
+        //     }
+        // }
+        // return max;
     }
 }
